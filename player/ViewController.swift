89a4013375fb9ada0e1ae5f22e98b7b78219b678
morfius964 <morfius964@hotmail.com>
2016-12-25 15:38:56 -0500

    First Commit

---
+++ player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  player
+//
+//  Created by MCS on 12/25/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
